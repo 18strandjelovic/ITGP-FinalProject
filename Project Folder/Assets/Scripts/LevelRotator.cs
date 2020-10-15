47f8988b79482f620b5c091706d0201f72aa59ab
bsimon12 <bsimon12@kent.edu>
2020-10-15 10:40:53 -0400

    Initial rotation system

---
+++ Project Folder/Assets/Scripts/LevelRotator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LevelRotator : MonoBehaviour {
+    public float rotationSensitivity;
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnMove(InputValue value) {
+        var rotation = value.Get<Vector2>();
+
+        var xRotation = rotation.y * rotationSensitivity * 1.5f * Time.deltaTime;
+        var yRotation = -rotation.x * rotationSensitivity * Time.deltaTime;
+
+        transform.Rotate(new Vector3(xRotation, 0, yRotation));
+    }
+}
